ce6fddd8e189225d935a97e8c48b29376f248d12
HugoRCD <hugo.richard@epitech.eu>
2025-04-28 13:42:03 +0200

    feat: add projects

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+const { data: page } = await useAsyncData('index', () => {
+  return queryCollection('index').first()
+})
+if (!page.value) {
+  throw createError({
+    statusCode: 404,
+    statusMessage: 'Page not found',
+    fatal: true
+  })
+}
 
+useSeoMeta({
+  title: page.value?.seo.title,
+  ogTitle: page.value?.seo.title,
+  description: page.value?.seo.description,
+  ogDescription: page.value?.seo.description
+})
 </script>
 
 <template>
-  <div>
-    projects
-  </div>
+  <UPage>
+    <UPageHero title="Projects" />
+  </UPage>
 </template>
 
 <style scoped>
