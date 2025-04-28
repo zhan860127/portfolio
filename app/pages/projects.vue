c38241cef79c82e5a92612abbf855141178c4831
HugoRCD <hugo.richard@epitech.eu>
2025-04-28 16:09:27 +0200

    up

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -47,37 +47,62 @@ useSeoMeta({
             color="neutral"
             v-bind="page.links[0]"
           />
+          <UButton
+            v-bind="page.links[1]"
+          />
         </div>
       </template>
     </UPageHero>
-    <UContainer>
-      <UPageGrid class="sm:!grid-cols-2">
-        <Motion
-          v-for="(project, index) in projects"
-          :key="project.title"
-          :initial="{ opacity: 0, transform: 'translateY(10px)' }"
-          :while-in-view="{ opacity: 1, transform: 'translateY(0)' }"
-          :transition="{ delay: 0.1 * index }"
-          :in-view-options="{ once: true }"
+    <UPageSection
+      :ui="{
+        container: '!pt-0'
+      }"
+    >
+      <Motion
+        v-for="(project, index) in projects"
+        :key="project.title"
+        :initial="{ opacity: 0, transform: 'translateY(10px)' }"
+        :while-in-view="{ opacity: 1, transform: 'translateY(0)' }"
+        :transition="{ delay: 0.1 * index }"
+        :in-view-options="{ once: true }"
+      >
+        <UPageCard
+          :title="project.title"
+          :description="project.description"
+          :to="project.url"
+          orientation="horizontal"
+          variant="naked"
+          :reverse="index % 2 === 1"
+          class="group"
+          :ui="{
+            wrapper: 'max-sm:order-last'
+          }"
         >
-          <UPageCard
-            :title="project.title"
-            :description="project.description"
-            :to="project.url"
-            orientation="vertical"
-            reverse
-            :ui="{
-              wrapper: 'order-last'
-            }"
-          >
-            <img
-              :src="project.image"
-              :alt="project.title"
-              class="object-cover w-full h-48 rounded-lg"
+          <template #leading>
+            <span class="text-sm text-muted">
+              {{ project.date }}
+            </span>
+          </template>
+          <template #footer>
+            <ULink
+              :to="project.url"
+              class="text-sm text-primary flex items-center"
             >
-          </UPageCard>
-        </Motion>
-      </UPageGrid>
-    </UContainer>
+              View Project
+              <UIcon
+                name="i-lucide-arrow-right"
+                class="size-4 text-primary transition-all opacity-0 group-hover:translate-x-1 group-hover:opacity-100"
+              />
+            </ULink>
+          </template>
+          <img
+            :src="project.image"
+            :alt="project.title"
+            class="object-cover w-full h-48 rounded-lg mask-b-from-50% mask-b-to-100%"
+            :class="index % 2 === 1 ? 'mask-l-from-50% mask-l-to-100%' : 'mask-r-from-50% mask-r-to-100%'"
+          >
+        </UPageCard>
+      </Motion>
+    </UPageSection>
   </UPage>
 </template>
