5ce8a67aae6d5e87d334e3993194f90bfd420f11
HugoRCD <hugo.richard@epitech.eu>
2025-04-29 15:59:37 +0200

    up

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div>
-    <UContainer class="border-x border-default mt-10">
+    <UContainer class="border-x border-default pt-10">
       <AppHeader :links="navLinks" />
       <slot />
       <AppFooter :links="navLinks" />
