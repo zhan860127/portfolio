056e31e3ac581b60627d909b760c8c8572ab1fbd
Vachmara <valentinchmara@gmail.com>
2025-05-05 14:57:59 +0200

    fix: more than 3 testimonials break UI (#10)
    Co-authored-by: Hugo Richard <hugo.richard@epitech.eu>

--- app/components/landing/Testimonials.vue
+++ app/components/landing/Testimonials.vue
@@ -19,7 +19,7 @@ defineProps<{
       loop
       dots
       :ui="{
-        viewport: '-mx-4 sm:-mx-12 lg:-mx-16 bg-elevated/50'
+        viewport: '-mx-4 sm:-mx-12 lg:-mx-16 bg-elevated/50 max-w-(--ui-container)'
       }"
     >
       <UPageCTA
