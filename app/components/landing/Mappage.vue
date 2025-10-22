95e13d2e2d113d9d82bb00e88174ff9ad3c75db8
zhan860127 <zhan860127@gmail.com>
2025-10-22 13:45:39 +0800

    add the map to the index page

---
+++ app/components/landing/Mappage.vue
@@ -0,0 +1,35 @@
+
+<script setup lang="ts">
+
+const svgRef = ref<InstanceType<typeof LandingMap> | null>(null)
+
+onMounted(() => {
+
+
+  if (svgRef.value) {
+    // 拿到原生 svg DOM
+    const svgEl = (svgRef.value as any).$el as SVGSVGElement
+    const north = svgEl.querySelectorAll('#TWHSQ')
+    console.log(north)
+    for(let k of north){
+        k.setAttribute('fill', 'blue')
+    }
+    console.log(TWKIN)
+  
+  }
+
+})
+</script>
+
+<template>
+    <UPageSection
+      :ui="{
+        container: '!pt-0 lg:grid lg:gap-8 w-full'
+      }"
+    >
+    <LandingMap class="w-full" ref = "svgRef">
+
+    </LandingMap>
+</UPageSection>
+</template>
+
