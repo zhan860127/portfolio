b39d32fb0307c8e698d34bef1c0b6f689ad551c7
zhan860127 <zhan860127@gmail.com>
2025-10-22 15:36:35 +0800

    change the color

--- app/components/landing/Mappage.vue
+++ app/components/landing/Mappage.vue
@@ -3,21 +3,17 @@
 
 const svgRef = ref<InstanceType<typeof LandingMap> | null>(null)
 
-onMounted(() => {
-
-
+onMounted(() => {   
   if (svgRef.value) {
     // 拿到原生 svg DOM
     const svgEl = (svgRef.value as any).$el as SVGSVGElement
-    const north = svgEl.querySelectorAll('#TWHSQ')
-    console.log(north)
+    const north = svgEl.querySelectorAll('#TWHSQ') 
     for(let k of north){
-        k.setAttribute('fill', 'blue')
-    }
-    console.log(TWKIN)
-  
-  }
+        k.classList.add("lable")
+        k.classList.add("show")
 
+    }
+  }
 })
 </script>
 
@@ -32,4 +28,14 @@ onMounted(() => {
     </LandingMap>
 </UPageSection>
 </template>
+<style >
+.lable{
+    fill:#a7be8c !important;
+   opacity: 0;
+  transition: all 0.6s ease-out;
+}
+.lable.show {
+  opacity: 1;
 
+}
+</style>
